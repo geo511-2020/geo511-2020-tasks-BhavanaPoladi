44c4868701cb27104b8fc146749bddc2f4338535
BhavanaPoladi <bhavanap@buffalo.edu>
2020-10-18 05:52:32 -0400

    Case study 1

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,1 +1,9 @@
+rm(list=ls())
+setwd("/Users/bhavanapoladi/Desktop/GEO511")
+library(datasets)
 data(iris)
+summary(iris)
+iris$Petal.Length
+petal_length_mean <- mean(iris$Petal.Length)
+petal_length_mean
+hist(iris$Petal.Length)
